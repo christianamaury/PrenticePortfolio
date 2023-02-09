25ede2e35d5a6b555cb649e96107f86391d51a6b
christianamaury <chris.amaury@hotmail.com>
2023-02-08 17:57:57 -0700

    intro component css

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import {Link} from "react-scroll";
 import './intro.css';
 import bg from "../../assets/image.png";
+import btnImg from "../../assets/hireme.png";
 
 export const Intro = () => {
   return(
@@ -15,12 +17,25 @@ export const Intro = () => {
         </span>
 
         <span className='introName'>
-          Prentice
+          Prentice Ahmad
           <br/>
-          Web Designer
+
         </span>
 
-    
+        <p className='introPara'>
+         Formerly known as (Prynce Michael) is one of the most vivacious speakers you will ever hear.
+         He is the host of "The Chat" podcast, as well as Two-time International Best-selling Author of books such as;
+         "No Time Like The Present", "I Am A Flight Attendant And That Is My Superpower", and "The Handbook to Developing Human Equity: World Civility - The Missing Element".
+         Prentice Ahmad is also THE FIRST MISTER U.S. UNITED 2023 crowned July 2023. From homeless, being on public assistance to CEO entrepreneur, Prentice Ahmad's transparency and charismatic coaching has influenced audiences worldwide proving that we all have the potential to be great,
+         but it is what we do with that potential that matters.
+         </p>
+
+         <Link>
+          <button className='btn'>
+            <img src={btnImg} alt="Hire Me Photo" className='btnImg'/>
+              Hire Me
+          </button>
+         </Link>
 
       </div>
 
